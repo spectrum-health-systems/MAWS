1aeb7ed5be845187d6f45c90778902fd94b36167
Chris Banwarth <APrettyCoolProgram@users.noreply.github.com>
2022-06-27 08:37:52 -0400

    220627.083749-devbuild

--- src/MAWS.asmx.cs
+++ src/MAWS.asmx.cs
@@ -4,7 +4,7 @@
 // https://github.com/spectrum-health-systems/MAWSC)
 // Apache v2 (https://apache.org/licenses/LICENSE-2.0)
 // Copyright 2021-2022 A Pretty Cool Program
-// ================================================================= [ 220624.122818-devbuild ]
+// ================================================================= [ 220627.083542-devbuild ]
 
 // MAWS.asmx.cs
 // Entry point for MAWS.
