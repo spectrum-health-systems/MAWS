37aa51b5d02726bc892d118497648f6b57978d30
Chris Banwarth <APrettyCoolProgram@users.noreply.github.com>
2022-07-22 08:54:38 -0400

    Refresh

---
+++ Tools/README.md
@@ -0,0 +1,3 @@
+# Tools
+
+Nothing here yet.
---
+++ src/Data/Validate.cs
@@ -0,0 +1,28 @@
+﻿// ============================================================================================
+// MAWS: MyAvatar Web Service
+// A custom web service for Netsmart's myAvatar™ EHR.
+// https://github.com/spectrum-health-systems/MAWSC)
+// Apache v2 (https://apache.org/licenses/LICENSE-2.0)
+// Copyright 2021-2022 A Pretty Cool Program
+// ============================================================================================
+
+// MAWS.Data.Validate.cs
+// Logic dealing with MAWS data.
+// b220624.115605
+// https://github.com/spectrum-health-systems/MAWS/blob/main/Documentation/Sourcecode/MAWS-Sourcecode.md
+
+namespace MAWS
+{
+    public class Validate
+    {
+        ///// <summary>Validate the Avatar username.</summary>
+        ///// <param name="avatarUserName">The value of sentOptObj.OptionUserId.</param>
+        ///// <returns>A valid username.</returns>
+        //public static string AvatarUserName(string avatarUserName)
+        //{
+        //    return string.IsNullOrWhiteSpace(avatarUserName)
+        //        ? Properties.Settings.Default.FallbackAvatarUserName
+        //        : avatarUserName;
+        //}
+    }
+}
